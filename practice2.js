a1ae23a1aca436009d40c81252f00b117344ed09
Rachel Hill <hillrachel187@gmail.com>
2021-12-16 17:17:34 -0600

    Update correct practice2 file

---
+++ practice2.js
@@ -0,0 +1,17 @@
+// objects
+// declare an object named rachel and leave it empty
+var rachel = {
+// end curly brace for object assignment
+}
+
+// assign a new a new key value pair named age and assign it a value of 26 to the object you created above.
+rachel.age = 26;
+// return your object
+console.log(rachel);
+
+rachel.pronouns = 'she/her';
+console.log(rachel);
+
+// declare a variable and assign it the string with quoutes and quote something within it.
+var quote = 'Eli said, "don\'t mess this up!"';
+console.log(quote);
